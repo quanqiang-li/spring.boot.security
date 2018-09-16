2b8a3053b1140d403df15414010ee258a362ad2c
quanqiang-li <2476056494@qq.com>
2018-09-16 20:30:33 +0800

    oauth

---
+++ src/main/java/spring/boot/oauth/Application.java
@@ -0,0 +1,20 @@
+package spring.boot.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * oauth登录
+ * {@link https://www.cnblogs.com/cjsblog/category/1235055.html }
+ * @author carl
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
